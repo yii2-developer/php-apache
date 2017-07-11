27ffe1d415ef9dc080c048a89e9a474b7c412f47
yii2-developer <yii2.developer@gmail.com>
2017-07-11 17:24:56 +0500

    fix: cron

--- 7.1/init.sh
+++ 7.1/init.sh
@@ -2,10 +2,12 @@
 
 supervisord -c /etc/supervisor/supervisord.conf
 
+USER_ID=${APACHE_RUN_ID:-33}
+
 if [ -n "$APACHE_RUN_USER" ]
 then
     if ! id "$APACHE_RUN_USER" >/dev/null 2>&1; then
-            useradd --shell=/bin/bash --home-dir="/var/www" "$APACHE_RUN_USER"
+            useradd --shell=/bin/bash --home-dir="/var/www" --uid "$USER_ID" --gid "$USER_ID" --user-group --non-unique "$APACHE_RUN_USER"
     fi
 
     CRON_FILE="/root/cron/tabs"
