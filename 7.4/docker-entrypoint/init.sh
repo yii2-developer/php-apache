f05e71aab2ac785203afaf19075f2fe8f031885c
Alexander <yii2.developer@gmail.com>
2020-03-27 10:42:45 +0500

    php:7.4

---
+++ 7.4/docker-entrypoint/init.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+supervisord -c /etc/supervisor/supervisord.conf
+
+USER_ID=${APACHE_RUN_ID:-33}
+
+if [ -n "$APACHE_RUN_USER" ]
+then
+    if ! id "$APACHE_RUN_USER" >/dev/null 2>&1; then
+        groupadd --gid="$USER_ID" --non-unique "$APACHE_RUN_USER"
+        useradd --shell=/bin/bash --home-dir="/var/www" --uid="$USER_ID" --gid="$USER_ID" --non-unique "$APACHE_RUN_USER"
+    fi
+
+    if [ -f "$CRON_FILE" ]
+    then
+        crontab -u "$APACHE_RUN_USER" "$CRON_FILE"
+    fi
+fi
+
+su --command="supervisord -c /etc/supervisor/application.conf" "$APACHE_RUN_USER"
+
+apache2-foreground
