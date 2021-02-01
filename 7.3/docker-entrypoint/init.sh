05314d98f8999630895521baea426200361fd006
Alexander <yii2.developer@gmail.com>
2021-02-01 12:50:25 +0500

    fix: init.sh

--- 7.3/docker-entrypoint/init.sh
+++ 7.3/docker-entrypoint/init.sh
@@ -4,19 +4,17 @@ supervisord -c /etc/supervisor/supervisord.conf
 
 USER_ID=${APACHE_RUN_ID:-33}
 
-if [ -n "$APACHE_RUN_USER" ]
-then
-    if ! id "$APACHE_RUN_USER" >/dev/null 2>&1; then
-        groupadd --gid="$USER_ID" --non-unique "$APACHE_RUN_USER"
-        useradd --shell=/bin/bash --home-dir="/var/www" --uid="$USER_ID" --gid="$USER_ID" --non-unique "$APACHE_RUN_USER"
-    fi
+if [ -n "$APACHE_RUN_USER" ]; then
+  if ! id "$APACHE_RUN_USER" >/dev/null 2>&1; then
+    groupadd --gid="$USER_ID" --non-unique "$APACHE_RUN_USER"
+    useradd --shell=/bin/bash --home-dir="/var/www" --uid="$USER_ID" --gid="$USER_ID" --non-unique "$APACHE_RUN_USER"
+  fi
 
-    if [ -f "$CRON_FILE" ]
-    then
-        crontab -u "$APACHE_RUN_USER" "$CRON_FILE"
-    fi
+  if [ -f "$CRON_FILE" ]; then
+    crontab -u "$APACHE_RUN_USER" "$CRON_FILE"
+  fi
+
+  su --command="supervisord --configuration /etc/supervisor/application.conf" "$APACHE_RUN_USER"
 fi
-
-su --command="supervisord -c /etc/supervisor/application.conf" "$APACHE_RUN_USER"
 
 apache2-foreground
