df0384736fc2c7c499407df75655e4e5bbcf1fab
Alexander <yii2.developer@gmail.com>
2021-03-06 13:10:53 +0500

    fix: init.sh

--- 7.4/docker-entrypoint/init.sh
+++ 7.4/docker-entrypoint/init.sh
@@ -4,10 +4,10 @@ set -e
 
 supervisord --configuration /etc/supervisor/supervisord.conf
 
-APACHE_RUN_ID=${APACHE_RUN_ID:-33}
-
 if [ -n "$APACHE_RUN_USER" ]; then
   if ! id "$APACHE_RUN_USER" >/dev/null 2>&1; then
+    APACHE_RUN_ID=${APACHE_RUN_ID:-33}
+
     groupadd --gid="$APACHE_RUN_ID" --non-unique "$APACHE_RUN_USER"
     useradd --shell=/bin/bash --home-dir="/var/www" --uid="$APACHE_RUN_ID" --gid="$APACHE_RUN_ID" --non-unique "$APACHE_RUN_USER"
   fi
